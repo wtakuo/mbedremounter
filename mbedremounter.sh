2c09a6ee80d65176f70672a92826b6cc36c2e5c8
Takuo WATANABE <takuo@acm.org>
2014-12-18 17:20:31 +0900

    Removed echo line for debugging

--- mbedremounter.sh
+++ mbedremounter.sh
@@ -10,7 +10,6 @@
 for m in /Volumes/MBED* /Volumes/NUCLEO*; do
     if [ -d "$m" -a $(diskutil info "$m" | egrep -c 'Read-Only Volume.*Yes') == 1 ]; then
         d=$(df -l | egrep "$m"\$ | awk '{print $1}')
-        echo $d "$m"
         umount $d
         mkdir -p "$m"
         mount -w -o sync -t msdos $d "$m"
